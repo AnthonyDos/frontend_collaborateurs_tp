0b2d11e0a103bd5fca88f5db7fa60d423da364f1
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2023-02-09 19:08:28 +0100

    filtre par type

--- src/components/ListCollaborateurs.jsx
+++ src/components/ListCollaborateurs.jsx
@@ -6,7 +6,8 @@ const ListCollaborateurs = () => {
     const [ collaborators, setCollaborators ] = useState([])
     const [ filterData, setFilterData ] = useState([]);
     const [ queryCollaborator, setQueryCollaborator ] = useState("");
-    const [ typeSearch, setTypeSearch ] = useState("");
+    const [ typeSearch, setTypeSearch ] = useState("choice");
+    const [ categorySearch, setCategorySearch ] = useState("categorie");
 
     useEffect(()=> {
         const getUserData = async() => {
@@ -15,28 +16,71 @@ const ListCollaborateurs = () => {
             setFilterData(data)
         }
         getUserData()
-    },[])
+    },[typeSearch])    
+
+    const HandleSearchType = (e) => {
+        const filterSearchType = e.target.value;
+        setTypeSearch(filterSearchType)
+    }
+
+    const HandleSearchCategorie = (e) => {
+        const filterSearchCategory = e.target.value;
+        setCategorySearch(filterSearchCategory)
+    }
 
     const HandleSearch = (e) => {
+        
         const getSearch = e.target.value;
         
-        if (getSearch.length > 0) {
-            console.log(typeSearch)
+        if (getSearch.length > 0 && categorySearch === "categorie" && typeSearch === "choice") {
+
             const searchData = collaborators.filter((collaborator)=> 
-                
-                collaborator.firstname.toLowerCase().includes(getSearch)); 
-                console.log(searchData)
+                collaborator.lastname.toLowerCase().includes(getSearch)); 
             setCollaborators(searchData);
-        }else {
+
+        }else if (getSearch.length > 0 && typeSearch === "lastname") {
+
+            const searchData = collaborators.filter((collaborator)=> 
+                collaborator.lastname.toLowerCase().includes(getSearch)); 
+            setCollaborators(searchData);
+
+        }else if (getSearch.length > 0 && typeSearch === "category") {
+
+            const searchData = collaborators.filter((collaborator)=> 
+                collaborator.category.toLowerCase().includes(getSearch)); 
+            setCollaborators(searchData);
+
+        }else if (getSearch.length > 0 && typeSearch === "city") {
+
+            const searchData = collaborators.filter((collaborator)=> 
+                collaborator.city.toLowerCase().includes(getSearch)); 
+            setCollaborators(searchData);
+
+        }else if (getSearch.length > 0 && categorySearch === "Marketing") {
+
+            const searchData = collaborators.filter((collaborator)=> 
+                collaborator.lastname.toLowerCase().includes(getSearch) && collaborator.category === categorySearch || 
+                collaborator.firstname.toLowerCase().includes(getSearch) && collaborator.category === categorySearch); 
+            setCollaborators(searchData);
+
+        }else if (getSearch.length > 0 && categorySearch === "Technique") {
+
+            const searchData = collaborators.filter((collaborator)=> 
+                collaborator.lastname.toLowerCase().includes(getSearch) && collaborator.category === categorySearch || 
+                collaborator.firstname.toLowerCase().includes(getSearch) && collaborator.category === categorySearch); 
+            setCollaborators(searchData);
+
+        }else if (getSearch.length > 0 && categorySearch === "Client") {
+
+            const searchData = collaborators.filter((collaborator)=> 
+                collaborator.lastname.toLowerCase().includes(getSearch) && collaborator.category === categorySearch || 
+                collaborator.firstname.toLowerCase().includes(getSearch) && collaborator.category === categorySearch); 
+            setCollaborators(searchData);
+
+        }else{
             setCollaborators(filterData);
         }
         setQueryCollaborator(getSearch)
-    }
-
-    const HandleSearchLastname = (e) => {
-        const filterSearchType = e.target.value;
-        console.log(filterSearchType)
-        setTypeSearch(filterSearchType)
     }
 
     return(
@@ -47,12 +91,23 @@ const ListCollaborateurs = () => {
                 </form>
                 <p>Recherche par nom :</p>
                 <select 
-                    onChange={(e)=> HandleSearchLastname(e)}
-                    defaultValue={"nom"}
+                    onChange={(e)=> HandleSearchType(e)}
+                    defaultValue={"choice"}
                 >
-                    <option value="nom">Nom</option>
-                    <option value="categorie">Catégorie</option>
-                    <option value="localisation">Localisation</option>
+                    <option value="choice">choix</option>
+                    <option value="lastname">Nom</option>
+                    <option value="category">Catégorie</option>
+                    <option value="city">Localisation</option>
+                </select>
+                <p>Catégorie</p>
+                <select 
+                    onChange={(e)=> HandleSearchCategorie(e)} 
+                    defaultValue="categorie"
+                >
+                    <option value="categorie">catégories</option>
+                    <option value="Marketing">Marketing</option>
+                    <option value="Technique">Technique</option>
+                    <option value="Client">Client</option>
                 </select>
             </div>
             <div className="container_list_collaborators">
